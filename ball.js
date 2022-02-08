aa385514a9e8645cdff84de72cfcb3ffaafc176e
Vince Hartman <97495456+vhartman11@users.noreply.github.com>
2022-02-08 17:51:59 -0500

    Add files via upload

--- ball.js
+++ ball.js
@@ -1,44 +1,21 @@
 //Set global variable that would contain the position, velocity and the html element "ball"
-
-var velocity = 100;
-var positionX = 0;
-var positionY = 0;
+var position = 0;
+var velocity = 10;
 var reverse = false;
-var ball = document.getElementById("ball");
+var ball = document.getElementById('ball');
 
-//write a function that can change the position of the html element "ball"
+function moveBall(){
+    if(!reverse){
+        position = position + velocity;
+        ball.style.left = position + "px";
+    } else {
+        position = position - velocity;
+        ball.style.left = position + "px";
+    }
 
-function moveBall() {
-  positionX = positionX + velocity;
-  console.log("positionX: " + positionX)
-  // two fixed x-axis coordinates (you will use these variable in step 3)
-  var Xmin = 0;
-  var Xmax = 300;
-  var Ymin = 0;
-  var Ymax = 300
-
-  if (reverse) {
-    positionX = positionX - velocity;
-    positionY = positionY - velocity;
-    ball.style.top = positionY + "px";
-    ball.style.left = positionX + "px";
-  } else {
-    if (reverse) {
-    positionX = positionX - velocity;
-    positionY = positionY - velocity;
-    ball.style.top = positionY + "px";
-    ball.style.left = positionX + "px";
-  } else {
-
-  if(
-    positionX > Xmax ||
-    positionX === Xmin ||
-    positionY > Ymax ||
-    positionY === Ymin
-  ){
+if(position > 300 || positsion == 0){
     reverse = !reverse;
-  }
+}    
 }
 
-// This call the moveBall function every 100ms
 setInterval(moveBall, 100);
